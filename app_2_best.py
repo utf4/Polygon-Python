b12e7b90178e0053fa64ce57e7728cd2a09cc4bd
Darkness <darkness@Darkness>
2023-02-07 08:53:09 +0500

    Python polygon initail commit

---
+++ app_2_best.py
@@ -0,0 +1,71 @@
+from flask import Flask, request, jsonify
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+	return "All OK!"
+
+@app.route("/polygons", methods=["GET", "POST"])
+def polyggon_handler():
+    if request.method == "GET":
+        polygs = get_all_polygons()
+        return jsonify(polygons=polygs)
+    elif request.method == "POST":
+        polygon = request.get_json()
+        save_polygon(polygon)
+        return "OK", 201
+
+@app.route("/polygons/<int:poly_id>", methods=["GET", "PUT", "DELETE"])
+def single_polygon_handler(poly_id):
+    if request.method == "GET":
+        poly = get_polygon(poly_id)
+        return jsonify(polygon=poly)
+    elif request.method == "PUT":
+        polygon = request.get_json()
+        update_polygon(poly_id, polygon)
+        return "OK", 200
+    elif request.method == "DELETE":
+        delete_polygon(poly_id)
+        return "OK", 200
+
+def get_all_polygons():
+    conn = sqlite3.connect("polygons.db")
+    c = conn.cursor()
+    c.execute("SELECT * FROM polygon")
+    polys = c.fetchall()
+    conn.close()
+    return polys
+
+def get_polygon(poly_id):
+    conn = sqlite3.connect("polygons.db")
+    c = conn.cursor()
+    c.execute("SELECT * FROM polygon WHERE id=?", (poly_id,))
+    poly = c.fetchone()
+    conn.close()
+    return poly
+
+def save_polygon(polygon):
+    conn = sqlite3.connect("polygons.db")
+    c = conn.cursor()
+    c.execute("INSERT INTO polygon (name, coordinates) VALUES (?, ?)", (polygon["name"], polygon["coordinates"]))
+    conn.commit()
+    conn.close()
+
+def update_polygon(poly_id, polygon):
+    conn = sqlite3.connect("polygons.db")
+    c = conn.cursor()
+    c.execute("UPDATE polygon SET name=?, coordinates=? WHERE id=?", (polygon["name"], polygon["coordinates"], poly_id))
+    conn.commit()
+    conn.close()
+
+def delete_polygon(poly_id):
+    conn = sqlite3.connect("polygons.db")
+    c = conn.cursor()
+    c.execute("DELETE FROM polygon WHERE id=?", (poly_id,))
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ init_db.py
@@ -0,0 +1,49 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_connection():
+    conn = None;
+    try:
+        conn = sqlite3.connect("./polygons.db")
+        print(f'successful connection with sqlite version {sqlite3.version}')
+    except Error as e:
+        print(e)
+    if conn:
+        return conn
+
+def create_table(conn, create_table_sql):
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+def initialize_data(conn, polygon):
+    sql = ''' INSERT INTO polygon(name,coordinates)
+                  VALUES(?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, polygon)
+    return cur.lastrowid
+
+def main():
+    database = "polygons.db"
+ 
+    sql_create_polygon_table = """ CREATE TABLE IF NOT EXISTS polygon (
+                                        id integer PRIMARY KEY,
+                                        name text NOT NULL,
+                                        coordinates text NOT NULL
+                                    ); """
+ 
+    conn = create_connection()
+    if conn is not None:
+        create_table(conn, sql_create_polygon_table)
+        polygon = ('polygon1', '[[0,0], [0,1], [1,1], [1,0], [0,0]]')
+        initialize_data(conn, polygon)
+        conn.commit()
+    else:
+        print("Error! Cannot create the database connection.")
+
+if __name__ == '__main__':
+    main()
+
+
